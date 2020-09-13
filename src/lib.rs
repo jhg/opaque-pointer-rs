64d50984f56f938977c05307007cf4189104ea77
Jesus Hernandez <jesushdez@protonmail.com>
2020-09-13 07:00:56 +0200

    Initial version to work with opaque pointers to expose Rust objects to C and C++

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+#![allow(unsafe_code)]
+#![warn(missing_docs)]
+#![warn(clippy::pedantic)]
+
+//! C FFI opaque pointers.
+//! 
+//! FFI to use Rust objects from C as opaque pointer.
+
+#[cfg(feature = "libc")]
+use libc::c_char;
+#[cfg(feature = "libc")]
+use std::ffi::CStr;
+
+/// Panic if a pointer is null.
+fn panic_if_null<T>(pointer: *const T) {
+    if pointer.is_null() {
+        unreachable!("A null pointer was passed to the library, something is wrong in the C or C++ code");
+    }
+}
+
+/// Convert type to raw pointer.
+pub fn raw<T>(data: T) -> *mut T {
+    let pointer = Box::into_raw(Box::new(data));
+    pointer
+}
+
+/// Free pointer to type.
+/// 
+/// **WARNING: never call it twice.**
+pub fn free<T>(pointer: *mut T) {
+    if pointer.is_null() {
+        #[cfg(debug_assertions)]
+        unreachable!("internal error: entered unreachable code");
+        #[cfg(not(debug_assertions))]
+        return;
+    }
+    unsafe { Box::from_raw(pointer) };
+    // We let drop the boxed data.
+}
+
+/// Own back from a raw pointer.
+/// 
+/// **WARNING: never call it twice.**
+pub fn own_back<T>(pointer: *mut T) -> T {
+    panic_if_null(pointer);
+    let boxed = unsafe {
+        Box::from_raw(pointer)
+    };
+    *boxed
+}
+
+/// Convert raw pointer to type to type reference.
+pub fn object<'a, T>(pointer: *const T) -> &'a T {
+    panic_if_null(pointer);
+    unsafe { &*pointer }
+}
+
+/// Convert raw pointer to type into type mutable reference.
+pub fn mut_object<'a, T>(pointer: *mut T) -> &'a mut T {
+    panic_if_null(pointer);
+    unsafe { &mut *pointer }
+}
+
+/// Reference to a C string.
+#[cfg(feature = "libc")]
+pub fn ref_str<'a>(string: *const c_char) -> &'a str {
+    panic_if_null(string);
+    let string = unsafe { CStr::from_ptr(string) };
+    string.to_str().expect("Invalid UTF-8 string from C or C++ code")
+}
---
+++ tests/pointer.rs
@@ -0,0 +1,52 @@
+use opaque_pointer;
+
+struct TestIt {
+    value: u8,
+}
+
+impl TestIt {
+    pub fn new(value: u8) -> Self {
+        Self {
+            value,
+        }
+    }
+    pub fn add(&mut self, value: u8) {
+        self.value += value;
+    }
+    pub fn get(&self) -> u8 {
+        self.value
+    }
+}
+
+#[test]
+fn opaque_pointer_with_free() {
+    let pointer = opaque_pointer::raw(TestIt::new(2));
+    {
+        let object = opaque_pointer::mut_object(pointer);
+        object.add(3);
+    }
+    {
+        let object = opaque_pointer::object(pointer);
+        assert_eq!(object.get(), 5);
+    }
+    {
+        opaque_pointer::free(pointer);
+    }
+}
+
+#[test]
+fn opaque_pointer_with_own_back() {
+    let pointer = opaque_pointer::raw(TestIt::new(2));
+    {
+        let object = opaque_pointer::mut_object(pointer);
+        object.add(3);
+    }
+    {
+        let object = opaque_pointer::object(pointer);
+        assert_eq!(object.get(), 5);
+    }
+    {
+        let test_it = opaque_pointer::own_back(pointer);
+        assert_eq!(test_it.get(), 5);
+    }
+}
