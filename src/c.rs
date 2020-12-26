5a03be91d2da8909986db7c54650f3a7863a91ff
Jesus Hernandez <jesushdez@protonmail.com>
2020-12-26 02:02:10 +0100

    fix: Add lifetime needed accidentally removed thinking it was not needed
    What was happening is that module did not run (nor compile) as c-types feature was not activated
    
    By default that feature is ok to be disabled but tests need to run with that feature and try to compile with it to detect errors

--- src/c.rs
+++ src/c.rs
@@ -20,7 +20,7 @@ use super::panic_if_null;
 /// If the C string is not a valid UTF-8 string.
 #[must_use]
 #[inline]
-pub unsafe fn ref_str(string: *const c_char) -> Result<&str, Utf8Error> {
+pub unsafe fn ref_str<'a>(string: *const c_char) -> Result<&'a str, Utf8Error> {
     #[cfg(any(feature = "panic-if-null", debug_assertions))]
     panic_if_null(string);
     // CAUTION: this is unsafe
