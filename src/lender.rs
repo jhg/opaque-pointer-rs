aced8f8986be1de7ae021e3c73ba96846c462723
Jesus Hernandez <jesushdez@protonmail.com>
2024-04-26 01:57:05 +0200

    refactor: Small changes and formatting

--- src/lender.rs
+++ src/lender.rs
@@ -1,8 +1,8 @@
 #![cfg(all(feature = "std", feature = "lender"))]
 
+use lazy_static::lazy_static;
 use std::collections::HashSet;
 use std::sync::{RwLock, RwLockWriteGuard};
-use lazy_static::lazy_static;
 
 lazy_static! {
     static ref LENT_POINTERS: RwLock<HashSet<usize>> = RwLock::new(HashSet::new());
@@ -23,7 +23,7 @@ pub(super) fn is_lent<T>(pointer: *const T) -> bool {
     return lent_pointers.contains(&(pointer as usize));
 }
 
-/// Add a pointer to list of lent pointers.
+/// Add a pointer to the list.
 ///
 /// # Panics
 ///
@@ -32,11 +32,10 @@ pub(super) fn is_lent<T>(pointer: *const T) -> bool {
 /// avoid panics while holding it.
 pub(super) fn lend<T>(pointer: *const T) {
     // Use try_reserve in nightly until it is available in stable
-    writable_lent_pointers()
-        .insert(pointer as usize);
+    writable_lent_pointers().insert(pointer as usize);
 }
 
-/// Remove a pointer from the list of lent pointers.
+/// Remove a pointer from the list.
 ///
 /// # Panics
 ///
@@ -44,13 +43,12 @@ pub(super) fn lend<T>(pointer: *const T) {
 /// while holding it. And it's specially reviewed and in a small module to
 /// avoid panics while holding it.
 pub(super) fn retrieve<T>(pointer: *const T) {
-    writable_lent_pointers()
-        .remove(&(pointer as usize));
+    writable_lent_pointers().remove(&(pointer as usize));
 }
 
 fn writable_lent_pointers() -> RwLockWriteGuard<'_, HashSet<usize>> {
     let Ok(lent_pointers) = LENT_POINTERS.write() else {
-        log::error!("RwLock poisoned, it is not possible to check pointers");
+        log::error!("RwLock poisoned, it is not possible to add or remove pointers");
         unreachable!("RwLock poisoned when there is not panics in code that can hold it");
     };
     return lent_pointers;
