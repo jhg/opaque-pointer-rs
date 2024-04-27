db51a3a92dde199ea0c3ac10ed87bde11376f953
Jesus Hernandez <jesushdez@protonmail.com>
2024-04-27 14:17:29 +0200

    fix: Add changes needed in commit of mode validation to submodule (human error)

--- src/lib.rs
+++ src/lib.rs
@@ -27,25 +27,7 @@ use error::PointerError;
 #[cfg(all(feature = "std", feature = "lender"))]
 mod lender;
 
-#[inline]
-fn validate_pointer_is_not_null<T>(pointer: *const T) -> Result<(), PointerError> {
-    if pointer.is_null() {
-        log::error!("Using a NULL pointer as an opaque pointer to Rust's data");
-        return Err(PointerError::Null);
-    }
-    return Ok(());
-}
-
-#[inline]
-fn validate_pointer<T>(pointer: *const T) -> Result<(), PointerError> {
-    validate_pointer_is_not_null(pointer)?;
-    #[cfg(all(feature = "std", feature = "lender"))]
-    if !lender::is_lent(pointer) {
-        log::error!("Using an invalid pointer as an opaque pointer to Rust's data");
-        return Err(PointerError::Invalid);
-    }
-    return Ok(());
-}
+mod validation;
 
 /// Get a heap-allocated raw pointer without ownership.
 ///
